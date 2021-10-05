dd0afb0b428ec275be96fedd920d43a173b1f2a6
mahines <marcus.hines@microsoft.com>
2021-10-05 13:41:09 -0400

    subscription id needs to be passed through as an arg

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM mcr.microsoft.com/azure-cli
 
 COPY . /action
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh -l
 
-export ARM_CLIENT_ID=$INPUT_ARM_CLIENT_ID
-
 export ACCOUNT_NAME=$INPUT_ACCOUNT_NAME
 export RESOURCE_GROUP_NAME=$INPUT_RESOURCE_GROUP_NAME
+export ARM_SUBSCRIPTION_ID=$INPUT_ARM_SUBSCRIPTION_ID
 export TF_VAR_resource_group_name=$INPUT_RESOURCE_GROUP_NAME
 
 echo "*******************"
 echo "Running entrypoint"
 echo "*******************"
 
-# Create a Cosmos DB account with default values and service endpoints
-# Use appropriate values for --kind or --capabilities for other APIs
-az cosmosdb create \
-    -n $ACCOUNT_NAME \
-    -g $RESOURCE_GROUP_NAME \
-    --enable-virtual-network true
+
+az cosmosdb create --name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription ARM_SUBSCRIPTION_ID
