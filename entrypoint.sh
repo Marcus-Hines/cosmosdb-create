a34f88bff60202be4a5756903ce0d18a092f652e
mahines <marcus.hines@microsoft.com>
2021-10-13 08:10:30 -0400

    fixing command order

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,9 @@ echo "Running entrypoint"
 echo "*******************"
 
 az login --service-principal -u $SP_USERNAME -p $SP_SECRET --tenant $ARM_TENANT_ID
-az cosmosdb sql role assignment create --account-name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --scope "/" --principal-id $MSI_OBJECT_ID --role-definition-id 00000000-0000-0000-0000-000000000001
 az cosmosdb create --name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $ARM_SUBSCRIPTION_ID
+az cosmosdb sql role assignment create --account-name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --scope "/" --principal-id $MSI_OBJECT_ID --role-definition-id 00000000-0000-0000-0000-000000000001
+
 
 echo "*******************"
 echo "Completed entrypoint"
