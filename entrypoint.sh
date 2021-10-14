d01d47916cb19055a205f4dbbc36d84a714c0fb6
mahines <marcus.hines@microsoft.com>
2021-10-14 13:36:31 -0400

    typo in my entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ echo "Assign role for cosmosdb"
 echo "*******************"
 
 az cosmosdb create --name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $ARM_SUBSCRIPTION_ID
-az cosmosdb sql role assignment create --account-name $ACCOUNT_NAME --resource-groxup $RESOURCE_GROUP_NAME --scope "/" --principal-id $MSI_OBJECT_ID --role-definition-id 00000000-0000-0000-0000-000000000001
+az cosmosdb sql role assignment create --account-name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --scope "/" --principal-id $MSI_OBJECT_ID --role-definition-id 00000000-0000-0000-0000-000000000001
 
 echo "*******************"
 echo "Assign MSI to VMSS"
