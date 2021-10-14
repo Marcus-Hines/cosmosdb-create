22724fc24265925a6e7c7bfa66d7ae86b957b9b7
mahines <marcus.hines@microsoft.com>
2021-10-14 09:05:36 -0400

    maybe the rg is incorrect.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ echo "Running entrypoint"
 echo "*******************"
 
 az login --service-principal -u $SP_USERNAME -p $SP_SECRET --tenant $ARM_TENANT_ID
+# may need to create the msi differently....like not on the cosmos db account. I dont know. The vmss identity command is not finding it
 az cosmosdb create --name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $ARM_SUBSCRIPTION_ID
 az cosmosdb sql role assignment create --account-name $ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME --scope "/" --principal-id $MSI_OBJECT_ID --role-definition-id 00000000-0000-0000-0000-000000000001
 
@@ -23,7 +24,7 @@ export REGION=$(az aks list --resource-group $RESOURCE_GROUP_NAME --query '[].lo
 export CLUSTER_NAME=$(az aks list --resource-group $RESOURCE_GROUP_NAME --query '[].name' -o tsv)
 export VMSS_NAME=$(az vmss list --resource-group MC_${RESOURCE_GROUP_NAME}_${CLUSTER_NAME}_${REGION} --query '[].name' -o tsv)
 
-az vmss identity assign --identities $ACCOUNT_NAME --name $VMSS_NAME --resource-group MC_${RESOURCE_GROUP_NAME}_${CLUSTER_NAME}_${REGION}
+az vmss identity assign --identities $ACCOUNT_NAME --name $VMSS_NAME --resource-group ${RESOURCE_GROUP_NAME}
 
 echo "*******************"
 echo "Completed entrypoint"
